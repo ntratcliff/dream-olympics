cd0e0faddf093ffb26f71f1b5ddd6137795a36c4
Noah Ratcliff <me@nratcliff.com>
2017-03-25 19:39:13 -0400

    jump

--- Dream Olympics/Assets/Scripts/Utility/JoystickMovement.cs
+++ Dream Olympics/Assets/Scripts/Utility/JoystickMovement.cs
@@ -2,12 +2,27 @@
 using System.Collections;
 
 [RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(Rigidbody))]
 public class JoystickMovement : MinigameBahaviour
 {
     public float SpeedMultiplier;
     public float RotationSpeed;
+    public float JumpForce;
+    public float InAirThreshold = 0.05f;
 
     private PlayerController playerController;
+
+    private float lastJumpVal;
+
+    private bool inAir
+    {
+        get
+        {
+            return Mathf.Abs(GetComponent<Rigidbody>().velocity.y) > InAirThreshold;
+        }
+    }
+
+    private bool veloCutThisJump;
 
     // Use this for initialization
     public override void Init(GameManager manager)
@@ -22,7 +37,6 @@ public class JoystickMovement : MinigameBahaviour
         if (GameRunning)
         {
             Rigidbody rb = GetComponent<Rigidbody>();
-            rb.velocity = Vector3.zero;
 
             Transform view = Camera.main.transform.parent.parent;
 
@@ -30,28 +44,49 @@ public class JoystickMovement : MinigameBahaviour
             float vertical = playerController.PlayerInfo.GetAxis("Vertical") * SpeedMultiplier;
             float horizontal = playerController.PlayerInfo.GetAxis("Horizontal") * SpeedMultiplier;
 
-            Vector3 direction = new Vector3(horizontal, 0, vertical);
-            direction = view.TransformDirection(direction);
+            Vector3 velocity = new Vector3(horizontal, 0, vertical);
+            velocity = view.TransformDirection(velocity);
 
+            // handle jump
+            float jumpVal = playerController.PlayerInfo.GetAxis("Action");
+            velocity.y = rb.velocity.y;
 
-            Vector3 velo = new Vector3(direction.x, rb.velocity.y, direction.z);
+            if(jumpVal > lastJumpVal && !inAir) // first time jump was pressed
+            {
+                Debug.Log("Jump pressed!");
 
-            rb.velocity = velo;
+                // add force
+                rb.AddForce(Vector3.up * JumpForce);
+
+                veloCutThisJump = false;
+            }
+            else if(jumpVal == lastJumpVal) // jump is held down
+            {
+
+            }
+            else if(jumpVal < lastJumpVal && inAir && !veloCutThisJump) // first time jump is released
+            {
+                Debug.Log("Jump released!");
+                // cut velocity in half
+                velocity.y /= 2;
+
+                veloCutThisJump = true;
+            }
+
+            lastJumpVal = jumpVal;
+
+            rb.velocity = velocity;
 
             //point player to movement direction
-            direction.Normalize();
-            if (direction != Vector3.zero)
+            // ignore y velocity
+            velocity.y = 0;
+            velocity.Normalize();
+            if (velocity != Vector3.zero)
             {
                 transform.rotation = Quaternion.Slerp(
                     transform.rotation,
-                    Quaternion.LookRotation(direction),
+                    Quaternion.LookRotation(velocity),
                     Time.deltaTime * RotationSpeed);
-            }
-
-            //report velocity to animator if it exists
-            if (GetComponent<Animator>() != null)
-            {
-                GetComponent<Animator>().SetFloat("VelocityMagnitude", velo.magnitude / SpeedMultiplier);
             }
         }
     }
